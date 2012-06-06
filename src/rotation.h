8385f9749024fb110dea4550c30d5342ad1df0a9
Snooperlock <teamworkucla@hotmail.com>
2012-06-06 11:42:16 -0700

    Update rotation performance testing

--- src/rotation.h
+++ src/rotation.h
@@ -4,13 +4,32 @@
 #include <omp.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include "IO/bmpfile.h"
 
 // min/max macro
 #define min(a, b)  (((a) < (b)) ? (a) : (b)) 
 #define max(a, b)  (((a) > (b)) ? (a) : (b))
 
+#define BILLION 1000000000.0
+
+struct timespec diff_timesp(struct timespec start, struct timespec end)
+{
+	struct timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = BILLION+end.tv_nsec-start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
 int rotation(char* in_image_path, double deg){
+    // start timer
+    clock_t start = clock();
+    
     // callibrate on deg
     while (deg >= 360.0)   deg -= 360.0;
     while (deg < 0.0)     deg += 360.0;
@@ -69,6 +88,11 @@ int rotation(char* in_image_path, double deg){
         printf("Fail to create rotation output image!\n");
         exit(1);
     }
+    
+    // Print the time before main loop
+    clock_t end_setup = clock();
+    double setup_time = end_setup - start;
+    printf("Setup time is %.3lfs\n", setup_time / CLOCKS_PER_SEC );
     
     int i,j;
     for (i = 0; i < out_width; ++i){
@@ -84,15 +108,30 @@ int rotation(char* in_image_path, double deg){
         }
     }
     
+    // Print the time after main loop
+    clock_t end_loop = clock();
+    double loop_time = end_loop - end_setup;
+    printf("Loop time is %.3lfs\n", loop_time / CLOCKS_PER_SEC );
+    
     bmp_save(output_img, "bmp_rotation_seq.bmp");
     
     bmp_destroy(input_img);
     bmp_destroy(output_img);
     
+    // Print the time after save image
+    clock_t end_save = clock();
+    double save_time = end_save - end_loop;
+    printf("Save time is %.3lfs\n", save_time / CLOCKS_PER_SEC );
+    printf("The ratio between saving and loop is %.3lf:1\n",  save_time/loop_time );
+    
     return 0;
 }
 
 int rotation_parallel(char* in_image_path, double deg, int num_of_threads, int chunk_size){
+    // define time struct
+    struct timespec start, end_setup, end_loop, end_save;
+    clock_gettime( CLOCK_REALTIME, &start);
+    
     // callibrate on deg
     while (deg >= 360.0)   deg -= 360.0;
     while (deg < 0.0)     deg += 360.0;
@@ -151,6 +190,11 @@ int rotation_parallel(char* in_image_path, double deg, int num_of_threads, int c
         printf("Fail to create rotation output image!\n");
         exit(1);
     }
+    
+    // before loop
+    clock_gettime( CLOCK_REALTIME, &end_setup);
+    struct timespec diff_time = diff_timesp(start, end_setup);
+    printf("Setup time is %.3lfs\n", (double)(diff_time.tv_sec+diff_time.tv_nsec/BILLION));
     
     // set the number of thread
     omp_set_num_threads(num_of_threads);
@@ -173,10 +217,21 @@ int rotation_parallel(char* in_image_path, double deg, int num_of_threads, int c
         }
     }
     
+    // after loop
+    clock_gettime( CLOCK_REALTIME, &end_loop);
+    struct timespec loop_time = diff_timesp(end_setup, end_loop);
+    printf("Loop time is %.3lfs\n", (double)(loop_time.tv_sec+loop_time.tv_nsec/BILLION));
+    
     bmp_save(output_img, "bmp_rotation_parallel.bmp");
     
     bmp_destroy(input_img);
     bmp_destroy(output_img);
+    
+    // after saving image
+    clock_gettime( CLOCK_REALTIME, &end_save);
+    struct timespec save_time = diff_timesp(end_loop, end_save);
+    printf("Save time is %.3lfs\n", (double)(save_time.tv_sec+save_time.tv_nsec/BILLION));
+    printf("The ratio between saving and loop is  %.3lf:1\n", ((double)(save_time.tv_sec+save_time.tv_nsec/BILLION))/((double)(loop_time.tv_sec+loop_time.tv_nsec/BILLION)));
     
     return 0;
 }
