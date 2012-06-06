2c67feee7e6fddc08202a796bf646f2eda1a9add
Fayryuka <Fayland@ucla.edu>
2012-06-05 17:13:38 -0700

    Finish Gaussian Blur parallel part

--- src/g_blur.h
+++ src/g_blur.h
@@ -6,8 +6,12 @@
 #include <math.h>
 #include "IO/bmpfile.h"
 
-#define MTX_ROW 3
-#define MTX_COL 3
+#define MTX_ROW 7
+#define MTX_COL 7
+
+// min/max macro
+#define min(a, b)  (((a) < (b)) ? (a) : (b)) 
+#define max(a, b)  (((a) > (b)) ? (a) : (b))
 
 double gaussian_func (int x, int y, double sigma){
     const double PI = atan(1.0)*4;
@@ -47,11 +51,22 @@ int gaussian_blur (char* in_image_path, double sigma){
     if (input_img){
         width = bmp_get_width(input_img);
         height = bmp_get_height(input_img);
+        printf("Input image width is %d, height is %d\n", width, height);
     }
     else {
         printf("Cannot read from file %s\n", in_image_path);
         exit(1);
     }
+    
+    // convert the input image into grayscale
+    rgb_pixel_t *pixel;
+    /*double *gray_mtx = (double *)malloc(sizeof(double) * width * height);
+    for (j = 0; j < height; ++j){
+        for (i = 0; i < width; ++i){
+            pixel = bmp_get_pixel(input_img, i, j);
+            gray_mtx[j*width+i] = pixel->red*0.3 + pixel->green*0.59 + pixel->blue*0.11;
+        }
+    }*/
     
     // create output image
     output_img = bmp_create(width, height, 8);
@@ -61,9 +76,9 @@ int gaussian_blur (char* in_image_path, double sigma){
     }
     
     //  apply Gaussian Blur
-    rgb_pixel_t *pixel;
     rgb_pixel_t out_pixel;
-    double clr_sum[4];      // 0: Red, 1: Green, 2: Blue, 3: Alpha
+    double clr_sum[4];
+    //double clr_sum;
     int count;
     
     for (j = 0; j < height; ++j){
@@ -71,6 +86,7 @@ int gaussian_blur (char* in_image_path, double sigma){
             // clear up the sum
             for (index = 0; index < 4; index++)
                 clr_sum[index] = 0.0;
+            //clr_sum = 0.0;
             
             // Blur the pixel by averaging the eight neighbour pixels
             for (row = 0; row < MTX_ROW; row++){
@@ -84,6 +100,7 @@ int gaussian_blur (char* in_image_path, double sigma){
                     clr_sum[1] += pixel->green * g_mtx[row*MTX_COL+col];
                     clr_sum[2] += pixel->blue * g_mtx[row*MTX_COL+col];
                     clr_sum[3] += pixel->alpha * g_mtx[row*MTX_COL+col];
+                    //clr_sum += gray_mtx[row*MTX_COL+col] * g_mtx[row*MTX_COL+col];
                 }
             }
             
@@ -92,7 +109,11 @@ int gaussian_blur (char* in_image_path, double sigma){
             out_pixel.green = clr_sum[1];
             out_pixel.blue  = clr_sum[2];
             out_pixel.alpha = clr_sum[3];
-            //pixel = bmp_get_pixel(input_img, i, j);
+            /*out_pixel.red   = max(0, min(255, clr_sum));
+            out_pixel.green = max(0, min(255, clr_sum));
+            out_pixel.blue  = max(0, min(255, clr_sum));
+            out_pixel.alpha = 255;*/
+            pixel = bmp_get_pixel(input_img, i, j);
             //printf("Previous R is %d, G is %d, B is %d, A is %d\n", pixel->red, pixel->green, pixel->blue, pixel->alpha);
             //printf("Current R is %d, G is %d, B is %d, A is %d\n", out_pixel.red, out_pixel.green, out_pixel.blue, out_pixel.alpha);
             bmp_set_pixel(output_img, i, j, out_pixel);
@@ -106,12 +127,111 @@ int gaussian_blur (char* in_image_path, double sigma){
     bmp_destroy(output_img);
     
     // free dynamic memory
-    if (g_mtx)  free(g_mtx);
+    if (g_mtx)      free(g_mtx);
+    //if (gray_mtx)   free(gray_mtx);
     
     return 0;
 }
 
 int gaussian_blur_parallel (char* in_image_path, double sigma, int num_of_threads, int chunk_size){
+    bmpfile_t *input_img = NULL, *output_img = NULL;
+    int width = 0, height = 0;
+    
+    // create Gaussian Kernal 3*3 matrix
+    double *g_mtx = (double *)malloc(sizeof(double) * MTX_COL * MTX_ROW);
+    g_mtx = gen_gaussian_kernal(g_mtx, sigma);
+    
+    // output the matrix
+    /*for (row = 0; row < MTX_ROW; row++){
+        for (col = 0; col < MTX_COL; col++){
+            printf("%lf ", g_mtx[row*MTX_COL+col]);
+        }
+        printf("\n");
+    }*/
+    
+    // read from file
+    input_img = bmp_create_8bpp_from_file(in_image_path);
+    
+    if (input_img){
+        width = bmp_get_width(input_img);
+        height = bmp_get_height(input_img);
+        printf("Input image width is %d, height is %d\n", width, height);
+    }
+    else {
+        printf("Cannot read from file %s\n", in_image_path);
+        exit(1);
+    }
+    
+    // convert the input image into grayscale
+    /*double *gray_mtx = (double *)malloc(sizeof(double) * width * height);
+    for (j = 0; j < height; ++j){
+        for (i = 0; i < width; ++i){
+            pixel = bmp_get_pixel(input_img, i, j);
+            gray_mtx[j*width+i] = pixel->red*0.3 + pixel->green*0.59 + pixel->blue*0.11;
+        }
+    }*/
+    
+    // create output image
+    output_img = bmp_create(width, height, 8);
+    if (!output_img) {
+        printf("Fail to create rotation output image!\n");
+        exit(1);
+    }
+    
+    //  apply Gaussian Blur
+    // set the number of thread
+    omp_set_num_threads(num_of_threads);
+    
+    #pragma omp parallel shared(output_img, g_mtx)
+    {
+        int i,j,row,col,index;
+        double clr_sum[4];
+        rgb_pixel_t *pixel;
+        rgb_pixel_t out_pixel;
+        
+        #pragma omp for schedule(dynamic, chunk_size) nowait
+        for (j = 0; j < height; ++j){
+            for (i = 0; i < width; ++i){
+                // clear up the sum
+                for (index = 0; index < 4; index++)
+                    clr_sum[index] = 0.0;
+                
+                // Blur the pixel by averaging the eight neighbour pixels
+                for (row = 0; row < MTX_ROW; row++){
+                    for (col = 0; col < MTX_COL; col++){
+                        int row_index = row+j-MTX_ROW/2, col_index = col+i-MTX_COL/2;
+                        if (row_index < 0 || row_index >= height 
+                            || col_index < 0 || col_index >= width)
+                            continue;
+                        pixel = bmp_get_pixel(input_img, col_index, row_index);
+                        clr_sum[0] += pixel->red * g_mtx[row*MTX_COL+col];
+                        clr_sum[1] += pixel->green * g_mtx[row*MTX_COL+col];
+                        clr_sum[2] += pixel->blue * g_mtx[row*MTX_COL+col];
+                        clr_sum[3] += pixel->alpha * g_mtx[row*MTX_COL+col];
+                    }
+                }
+                
+                // calculate the target pixel RGBA
+                out_pixel.red   = clr_sum[0];
+                out_pixel.green = clr_sum[1];
+                out_pixel.blue  = clr_sum[2];
+                out_pixel.alpha = clr_sum[3];
+                pixel = bmp_get_pixel(input_img, i, j);
+                bmp_set_pixel(output_img, i, j, out_pixel);
+            }
+        }
+    }
+    
+    // save the image
+    bmp_save(output_img, "gaussian_blur_parallel.bmp");
+    
+    bmp_destroy(input_img);
+    bmp_destroy(output_img);
+    
+    // free dynamic memory
+    if (g_mtx)      free(g_mtx);
+    
+    return 0;
 }
 
 #endif
